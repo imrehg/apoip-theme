1a8ecc807d04ae6b07a12abe9c8a0a52359ee5b9
Gergely Imreh <imrehg@gmail.com>
2015-02-25 18:09:45 +0800

    start to work on grid-page template

---
+++ wp-content/themes/apoip/page-templates/grid-page.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Template Name: Grid Page
+ *
+ * @package apoip
+ */
+
+get_header(); ?>
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', 'hero' ); ?>
+
+	<?php endwhile; ?>
+
+	<?php rewind_posts(); ?>
+
+	<?php if ( '' != $post->post_content ) : // only display if content not empty ?>
+
+	<div class="content-wrapper">
+
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!-- .content-wrapper -->
+
+	<?php endif; ?>
+
+	<?php
+		$child_pages = new WP_Query( array(
+			'post_type'      => 'page',
+			'orderby'        => 'menu_order',
+			'order'          => 'ASC',
+			'post_parent'    => $post->ID,
+			'posts_per_page' => 999,
+			'no_found_rows'  => true,
+		) );
+	?>
+
+	<div id="quaternary" class="grid-area">
+		<div class="grid-wrapper clear">
+
+	<?php if ( $child_pages->have_posts() ) : ?>
+
+			<?php while ( $child_pages->have_posts() ) : $child_pages->the_post(); ?>
+
+				<div class="grid">
+					<?php get_template_part( 'content', 'grid' ); ?>
+				</div><!-- .grid -->
+
+			<?php endwhile; ?>
+
+
+	<?php
+		endif;
+		wp_reset_postdata();
+	?>
+
+		</div><!-- .grid-wrapper -->
+	</div><!-- #quaternary -->
+
+<?php get_footer(); ?>
